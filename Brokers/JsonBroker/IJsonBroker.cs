095accd08121d465bd64bdea6f5c058e6750e44e
Sharaf M. Mansour <sharaf_1015@yahoo.com>
2024-05-18 06:00:25 +0300

    BROKERS: Add Json Broker

---
+++ Brokers/JsonBroker/IJsonBroker.cs
@@ -0,0 +1,5 @@
+﻿namespace OnlineExams.Brokers.JsonBroker;
+public partial interface IJsonBroker
+{
+    ValueTask<Exam> ReadExamAsync(string url);
+}
---
+++ Brokers/JsonBroker/JsonBroker.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineExams.Brokers.JsonBroker;
+public partial class JsonBroker(HttpClient httpClient) : IJsonBroker
+{
+    public async ValueTask<Exam> ReadExamAsync(string url)
+    {
+        var jsonString = await  httpClient.GetStringAsync(url);
+        return JsonSerializer.Deserialize<Exam>(jsonString) ?? new();
+    }
+}
