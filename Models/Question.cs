d9f6deb8e67052a24c5bbb841a0696bbe627646f
Sharaf M. Mansour <sharaf_1015@yahoo.com>
2024-05-18 05:58:58 +0300

    MODELS: Question Model

---
+++ Models/Question.cs
@@ -0,0 +1,8 @@
+﻿namespace OnlineExams.Models;
+public record Question(int ID, Answer[] Answers, string Value = "", ControlType Type = ControlType.RadioButton, bool IsCorrect = false, bool IsFlagged = false, int Num = 0)
+{
+    public bool IsCorrect { get; set; } = IsCorrect;
+    public bool IsFlagged { get; set; } = IsFlagged;
+    public Answer[] Answers { get; set; } = Answers;
+    public int Num { get; set; } = Num;
+}
