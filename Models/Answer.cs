e154dd2b20097e31a873c8b4e6d943ee1fd8bd99
Sharaf M. Mansour <sharaf_1015@yahoo.com>
2024-05-18 05:59:39 +0300

    MODELS: Answer Model

---
+++ Models/Answer.cs
@@ -0,0 +1,5 @@
+﻿namespace OnlineExams.Models;
+public record Answer(int ID, string Value, bool IsCorrect = false, bool IsSelected = false) 
+{
+    public bool IsSelected { get; set; } = IsSelected; 
+}
