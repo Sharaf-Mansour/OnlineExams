2134d0561ebf7d59e9c9ba62a78ac8b34cd2c124
Sharaf M. Mansour <sharaf_1015@yahoo.com>
2024-05-18 05:56:05 +0300

    FOUNDATIONS: Add ExamService

---
+++ Services/Foundation/ExamService.cs
@@ -0,0 +1,16 @@
+﻿namespace OnlineExams.Services.Foundation;
+public partial class ExamService(IJsonBroker jsonBroker) : IExamService
+{
+    public Exam Exam { get; private set; } = new();
+    public async ValueTask<Exam> RetrieveExamAsync(ExamCode type) => Exam = type switch
+    {
+        ExamCode.MTA98_361 => await jsonBroker.ReadExamAsync("MTA-98-361.json"),
+        ExamCode.AI900 or _ => await jsonBroker.ReadExamAsync("AI-900.json"),
+    };
+    public int RetrieveFlagCount() => Exam.Questions.Count(x => x.IsFlagged);
+    public double RetrieveScore() => Exam.Questions.Count(x => x.IsCorrect);
+    public int RetrieveQeustionsCount() => Exam.Questions.Length;
+    public double RetrieveAnsweredCount() => Exam.Questions.Count(x => x.Answers.Any(i => i.IsSelected));
+    public int RetrieveNotAnsweredCount() => Exam.Questions.Count(x => !x.Answers.Any(i => i.IsSelected));
+    public string RetrieveGrade() => (RetrieveScore() / RetrieveQeustionsCount() * 100).ToString("0.##") + "%";
+}
---
+++ Services/Foundation/IExamService.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlineExams.Services.Foundation;
+public partial interface IExamService
+{
+    Exam Exam { get; }
+    ValueTask<Exam> RetrieveExamAsync(ExamCode type);
+    int RetrieveFlagCount();
+    double RetrieveScore();
+    int RetrieveQeustionsCount();
+    double RetrieveAnsweredCount();
+    int RetrieveNotAnsweredCount();
+    string RetrieveGrade();
+}
