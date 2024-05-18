23f0bfbf8e4cc886ab13439f357e30d081a02b12
Sharaf M. Mansour <sharaf_1015@yahoo.com>
2024-05-18 06:01:02 +0300

    MAJOR FIX: Add Extensions for RestSelection and Shuffle

---
+++ Extensions/Helpercs.cs
@@ -0,0 +1,20 @@
+﻿namespace OnlineExams.Extensions;
+public static class Helpercs
+{
+    private static Random Random => new((int)DateTime.Now.Ticks);
+    public static void Shuffle<T>(this T[] list) 
+    {
+        int n = list.Length;
+        while (n > 1)
+        {
+            int k = Random.Next(n--);
+            (list[k], list[n]) = (list[n], list[k]);
+        }
+    }
+    public static void ResetSelection(this Question question)
+    {
+        for (int i = 0; i < question.Answers.Length; i++)
+            question.Answers[i].IsSelected = false;
+    }
+
+}
