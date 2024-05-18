2476750c39f5f85c23900544e857ed65046ed6a5
Sharaf M. Mansour <sharaf_1015@yahoo.com>
2024-05-18 05:57:01 +0300

    CONFIG: Add The DI And Import IT

--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using OnlineExams;
+global using OnlineExams.Models;
+global using System.Text.Json;
+global using OnlineExams.Brokers.JsonBroker;
+global using  OnlineExams.Extensions;
+global using Microsoft.AspNetCore.Components.Web;
+global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+global using OnlineExams;
+global using OnlineExams.Services.Foundation;
+global using Microsoft.AspNetCore.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddSingleton<IJsonBroker, JsonBroker>();
+builder.Services.AddSingleton<IExamService, ExamService>();
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new (builder.HostEnvironment.BaseAddress) });
 await builder.Build().RunAsync();
