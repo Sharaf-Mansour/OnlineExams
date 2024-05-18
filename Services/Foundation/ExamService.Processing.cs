86c6b4294b7f39581c7c38df11e534259d241f99
Sharaf M. Mansour <sharaf_1015@yahoo.com>
2024-05-18 05:56:23 +0300

    PROCESSING: Add ExamService Processing Layer

---
+++ Services/Foundation/ExamService.Processing.cs
@@ -0,0 +1,24 @@
+﻿namespace OnlineExams.Services.Foundation;
+public partial class ExamService : IExamService
+{
+    public bool AllowShowAnswer { get; set; }
+    public bool ShuffleAnswer { get; set; }
+    public bool ShuffleQeustion { get; set; }
+    public void SortQeustions() => Exam.Questions = [.. Exam.Questions.OrderBy(x => x.ID)];
+    public void SortAnswers()
+    {
+        for (int i = 0; i < RetrieveQeustionsCount(); i++)
+            Exam.Questions[i].Answers = [.. Exam.Questions[i].Answers.OrderBy(x => x.ID)];
+    }
+    public void ShuffleQeustions() => Exam.Questions.Shuffle();
+    public void ShuffleAnswers()
+    {
+        for (int i = 0; i < RetrieveQeustionsCount(); i++)
+            Exam.Questions[i].Answers.Shuffle();
+    }
+    public void AssignNumbers(int startNumber = 1)
+    {
+        for (int i = 0; i < RetrieveQeustionsCount(); i++)
+             Exam.Questions[i].Num = startNumber++;
+    }
+}
---
+++ Services/Foundation/IExamService.Processing.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlineExams.Services.Foundation;
+public partial interface IExamService
+{
+    bool AllowShowAnswer { get; set; }
+    bool ShuffleAnswer { get; set; }
+    bool ShuffleQeustion { get; set; }
+    void SortQeustions();
+    void SortAnswers();
+    void ShuffleQeustions();
+    void ShuffleAnswers();
+    void AssignNumbers(int startNumber = 1);
+}
